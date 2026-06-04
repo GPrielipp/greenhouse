4a322a6b43c74cb174729d2f7c3e371943257ab5
GPrielipp <goaliegeo30@gmail.com>
2026-06-04 11:04:54 -0400

    modified website look

---
+++ data/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require_once('../headers.php'); ?>
+<body>
+	<?php require_once('../navbar.php'); ?>
+	
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<!-- information about the website -->
-	<meta charset="UTF-8">
-	<title>Sensor Station</title>	
-	
-	<link rel="stylesheet" href="styles.css">
-
-</head>
+<?php require_once('headers.php'); ?>
 <body>
 <!-- the website -->
-	<h1>Welcome to your sensor dashboard!</h1>
-
+	<?php require_once('navbar.php'); ?>
+	<h1 class="center">Welcome to your sensor dashboard!</h1>
+	
+	<div class="center">	
+		<div class="card primary"> Test text to see what this looks like. </div>
+		<div class="card secondary"> Test text to see what this looks like. </div>
+		<div class="card accent"> Test text to see what this looks like. </div>
+		<div class="card warning"> Test text to see what this looks like. </div>
+		<div class="card error"> Test text to see what this looks like. </div>
+	</div>
 </body>
 </html>
