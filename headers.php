26fb0ff162eed39384965160fe3ed87c388fb9b9
GPrielipp <goaliegeo30@gmail.com>
2026-06-04 11:04:07 -0400

    breaking out common html elements into php to use require_once

---
+++ headers.php
@@ -0,0 +1,8 @@
+<head>
+<!-- information about the website -->
+        <meta charset="UTF-8">
+        <title>Sensor Station</title>
+
+        <link rel="stylesheet" href="/styles.css">
+
+</head>
---
+++ navbar.php
@@ -0,0 +1,5 @@
+<div class="navbar primary">
+	<a href="http://192.168.1.11"> Home </a>
+	<a href="/sensors"> Sensors </a>
+	<a href="/data"> Graphs </a>
+</div>
