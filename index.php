c9e3fcd5a2fb11ae6e951558c8b993d472d1b2fb
GPrielipp <goaliegeo30@gmail.com>
2026-06-04 09:47:46 -0400

    trying to get a boilerplate website down

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!-- information about the website -->
+	<meta charset="UTF-8">
+	<title>Sensor Station</title>	
+	
+	<link rel="stylesheet" href="styles.css">
+
+</head>
+<body>
+<!-- the website -->
+	<h1>Welcome to your sensor dashboard!</h1>
+
+</body>
+</html>
