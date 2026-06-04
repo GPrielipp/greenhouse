a232df55dd4f7a34999d0da6824fcfb468bf2d66
GPrielipp <goaliegeo30@gmail.com>
2026-06-04 09:47:27 -0400

    does all the hard work of 'deploying' my website

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 # move all php, html, css, js files to server
 webserver=/var/www/html/
 
+echo "copying all files to webserver"
 find . -type f -regextype posix-extended -regex ".*\.(php|css|html|js)" -print0 | while IFS= read -r -d '' file; do
 	# make sure the file exists
 	[ -e "$file" ] || continue
@@ -12,5 +13,6 @@ done
 # change permissions on the server
 # read (r), write (w), execute (x)
 # 644 = user=rw-  group=r--  others=r--
+echo "fixing permissions"
 find /var/www/html -type f -exec chmod 644 {} +
 
